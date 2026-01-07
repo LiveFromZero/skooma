d55aefd3e79db4105081fb85a552670c0a7c6967
Darleen <allerlei6@web.de>
2026-01-07 18:53:52 +0100

    Implemented SummaryText Component

--- skooma_frontend/src/SummaryText.tsx
+++ skooma_frontend/src/SummaryText.tsx
@@ -1,9 +1,15 @@
-import type { JSX } from "react";
+import { useState, type JSX } from "react";
 
 function SummaryText(): JSX.Element {
+  const [chosenYear, setChosenYear] = useState<string>("2024");
+  const [allAveragePercent, setAllAveragePercent] = useState<number>(75.6);
+
   return (
     <div>
-      <h2>Summary Text Component</h2>
+      <p>
+        Im Jahr {chosenYear} waren durchschnittlich {allAveragePercent}% aller
+        Rakenestarts erfolgreich.
+      </p>
     </div>
   );
 }
