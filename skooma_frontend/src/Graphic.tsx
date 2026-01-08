1eef1424a5c6e130d09d5e6f45cf44aa397e5589
Darleen <allerlei6@web.de>
2026-01-08 16:29:28 +0100

    added dummy implementation for api for graphic

--- skooma_frontend/src/Graphic.tsx
+++ skooma_frontend/src/Graphic.tsx
@@ -1,6 +1,6 @@
 import type { JSX } from "react";
 import { BarChart } from "@mui/x-charts/BarChart";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { FilterData } from "./Filter";
 
 interface RocketLaunchData {
@@ -22,6 +22,27 @@ function Graphic({ filterDaten }: { filterDaten: FilterData }): JSX.Element {
   const [dataOfRocketLaunches, setDataOfRocketLaunches] = useState<
     RocketLaunchData[]
   >([dummyData[0], dummyData[1], dummyData[2], dummyData[3]]);
+
+  useEffect(() => {
+    fetchData(filterDaten);
+  }, []);
+
+  function fetchData(filterDaten: FilterData): RocketLaunchData[] {
+    // Simulate data fetching based on filters
+    // filterDaten get send as parameters to backend in real application
+
+    // Dummy implementation:
+    if (!filterDaten.year && !filterDaten.rocketType) {
+      return dataOfRocketLaunches;
+    } else {
+      // Here you would normally fetch data from the backend using filterDaten
+      // setting the fetched data to state
+      var fetchedData: RocketLaunchData[] = dummyData; // in production, replace with fetched data
+      // here is the information from the backend set to refresh the graphic
+      setDataOfRocketLaunches(fetchedData);
+      return fetchedData;
+    }
+  }
 
   return (
     <div>
