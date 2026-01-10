e3f24c5ebd8c8c90b9bc81944b178ecdc7eb51fe
Darleen <allerlei6@web.de>
2026-01-10 20:53:40 +0100

    ADD: #3 added answer from backend for api calls

--- skooma_backend/Controllers/CommunicationToFrontendController.cs
+++ skooma_backend/Controllers/CommunicationToFrontendController.cs
@@ -9,22 +9,47 @@ namespace skooma_backend.Controllers
         [HttpGet]
         public IActionResult GetAvgSuccessfullRocketStarts(string year)
         {
+            // TODO START
+            // THIS is DUMMYDATA for API Call with frontend, needs to be changed to data from models and database
             double BackendDataToSendBack = 30.2;
 
-            double result = BackendDataToSendBack;
+            double result = BackendDataToSendBack; // needs to be adjustet when connected with backend
 
+            // TODO END
             return Ok(result);
         }
 
 
         // GET /rocket-starts/success-rate
-        [HttpGet("success-rate")]
-        public IActionResult GetAverageSuccessRate()
+        [HttpGet("rocketlaunchdata")]
+        public IActionResult GetRocketLaunchData()
         {
-            double result = 75.8; // Platzhalter, später DB
+            // TODO START
+            // THIS is DUMMYDATA for API Call with frontend, needs to be changed to data from models and database
+            
+            var backendDataToSendBack = new List<RocketLaunchStats>
+            {
+                new RocketLaunchStats { CountLaunches = 10, MoonPhase = 1, CountSuccessLaunches = 1 },
+                new RocketLaunchStats { CountLaunches = 10, MoonPhase = 2, CountSuccessLaunches = 1 },
+                new RocketLaunchStats { CountLaunches = 15, MoonPhase = 3, CountSuccessLaunches = 1 },
+                new RocketLaunchStats { CountLaunches = 15, MoonPhase = 4, CountSuccessLaunches = 1 }
+            };
+
+            var result = backendDataToSendBack; // needs to be adjustet when connected with backend
+            // TODO END
 
             return Ok(result);
         }
 
     }
+
+    // TODO This is a DUMMY-MODEL for API Calls with the frontend
+    // DELETE and use real model when connected to Database
+    public class RocketLaunchStats
+    {
+        public int CountLaunches { get; set; }
+        public int MoonPhase { get; set; }
+        public int CountSuccessLaunches { get; set; }
+    }
+
 }
