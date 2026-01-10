f6f7d0703d4d83c68c07214065b0c15f7b702fee
Darleen <allerlei6@web.de>
2026-01-10 21:29:14 +0100

    FIX: #3 forgot renamed variables

--- skooma_frontend/src/mock.tsx
+++ skooma_frontend/src/mock.tsx
@@ -4,8 +4,8 @@ import type { RocketLaunchData } from "./types";
 export const dummyAllAveragePercentage = 76.5;
 
 export const dummyRocketLaunchData: RocketLaunchData[] = [
-  { countLaunches: 5, moonphase: 1, countSuccessLaunches: 4 },
-  { countLaunches: 3, moonphase: 2, countSuccessLaunches: 2 },
-  { countLaunches: 7, moonphase: 3, countSuccessLaunches: 6 },
-  { countLaunches: 2, moonphase: 4, countSuccessLaunches: 1 },
+  { countLaunches: 5, moonPhase: 1, countSuccessLaunches: 4 },
+  { countLaunches: 3, moonPhase: 2, countSuccessLaunches: 2 },
+  { countLaunches: 7, moonPhase: 3, countSuccessLaunches: 6 },
+  { countLaunches: 2, moonPhase: 4, countSuccessLaunches: 1 },
 ];
