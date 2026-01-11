eda062d6917bff170dd8ced5e590e85d8a60e315
SEIF112 <Sebastian.Seifert@bertelsmann.de>
2026-01-11 17:50:25 +0100

    ADD: #4 DBContext for DB Ruleset

---
+++ skooma_backend/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using skooma_backend.Data.DBModels;
+
+namespace skooma_backend.Data
+{
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<DBLocation> Locations => Set<DBLocation>();
+        public DbSet<DBLaunch> Launches => Set<DBLaunch>();
+        public  DbSet<DBMoonData> MoonData => Set<DBMoonData>();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=library.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Fluent configuration (optional: conventions already handle this).
+            modelBuilder.Entity<DBLocation>()
+                .HasMany(a => a.Launches)
+                .WithOne(b => b.Location)
+                .HasForeignKey(b => b.LocationId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+
+}
--- skooma_backend/Data/DBModels/DBLaunch.cs
+++ skooma_backend/Data/DBModels/DBLaunch.cs
@@ -6,12 +6,14 @@ namespace skooma_backend.Data.DBModels
     public class DBLaunch
     {
         [Key]
-        public string Id { get; set; } = string.Empty;
+        public int Id { get; set; }
         public string RocketName { get; set; } = string.Empty;
         public DateTimeOffset LaunchDate { get; set; }
+        public string Status { get; set; } = string.Empty;
+
+        public int LocationId { get; set; }
 
         [ForeignKey(nameof(DBLocation.LocationId))]
         public DBLocation Location { get; set; } = null!;
-        public string Status { get; set; } = string.Empty;
     }
 }
